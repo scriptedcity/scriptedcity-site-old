def47325a7bbfd80103aef500d85e5f4e89774f2
millstones <500000ppm+github@gmail.com>
2023-01-07 17:03:33 +0900

    マージンの調整

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,9 @@
   <div>
     <ogp-head />
     <nav-bar />
-    <main class="md:pl-2 md:pr-2 lg:pl-12 lg:pr-12">
+    <main
+      class="md:pl-2 md:pr-2 lg:pl-12 lg:pr-12 xl:pl-20 xl:pr-20 2xl:pr-32 2xl:pl-32"
+    >
       <slot />
     </main>
     <app-footer class="fixed bottom-0" />
